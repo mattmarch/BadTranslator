5c0946ac5679e6cc542ed1ad958c50b493395bca
Matt March <matthewmarch29@gmail.com>
2020-05-08 12:08:34 +0100

    Print full path of languages translated rather than just latest language in output

--- translator.py
+++ translator.py
@@ -14,11 +14,13 @@ def translate_n_times(input_phrase, number_times):
     output = input_phrase
     phrase_in_current_language = input_phrase
     current_language = START_END_CODE
+    previous_languages = LANGUAGES[current_language]
     for next_language in translation_languages:
         phrase_in_current_language = translator.translate(phrase_in_current_language, src=current_language, dest=next_language).text
-        output += f'\n\n[{LANGUAGES[current_language]} -> {LANGUAGES[next_language]}]\n\n{phrase_in_current_language}'
+        previous_languages = f'{previous_languages} -> {LANGUAGES[next_language]}'
+        output += f'\n\n[{previous_languages}]\n\n{phrase_in_current_language}'
         current_language = next_language
-    return output        
+    return output
 
 if __name__ == '__main__':
     filename = sys.argv[1]
